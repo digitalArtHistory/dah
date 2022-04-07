6fbfd2492b3e87232376b232d95a9fecf2004a17
emchateau <emchateau@laposte.net>
2022-04-07 18:22:15 -0400

    ajout de page

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,2 +1,25 @@
-<h1>Page des projets</h1>
-<p>blala</p>
+<script>
+  export let item;
+  let projects = item.projects
+
+
+</script>
+
+<h1>{item.title}</h1>
+
+{#each projects as project }
+<article>
+  <h2>{project.title}</h2>
+  <p>Date : {project.date}</p>
+  <p><a href="{project.idno}">{project.idno}</a></p>
+  <iframe
+  id="inlineFrameExample"
+  title="Inline Frame Example"
+  width="300"
+  height="200"
+  src={project.idno}
+/>
+
+</article>
+
+{/each}
